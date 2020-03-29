bf3930b267f8e8e219849d69b7b2bb3d001e3a7b
Gustavo Faria <gustavofaria_1@hotmail.com>
2020-03-29 19:36:16 -0300

    mobile da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,50 @@
+// importar o módulo express para a variável express
+const express = require ('express');
+
+// importar rotas do arquivo routes.js
+const routes = require('./routes');
+
+// importar módulo cors
+const cors = require('cors');
+
+// variável que vai armazenar a minha aplicação
+const app = express();
+
+// informando cors
+app.use(cors());
+
+// informando ao corpo que usaremos json nas rotas post
+app.use(express.json());
+
+// informando as rotas
+app.use(routes);
+
+//ouvir a porta 3333
+app.listen(3333);
+
+/**
+ * Rota / Recurso
+ */
+/**
+ * Métodos HTTP:
+ * 
+ * GET: Buscar/listar uma informação do back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após o símbolo de ? e geralmente servem para filtros/paginação
+ * Rote Params: Parâmetros utilizados para identificar recursos
+ * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
+ */
+/**
+ * SQL (Structured Query Language): MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+// JSX (Java Script e XML)
+
+function App() {
+  return (
+    <Routes />
+
+  );
+}
+
+export default App;
